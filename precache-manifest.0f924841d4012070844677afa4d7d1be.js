2d8822d193920e1c424ed96960856017833a296e
Philipp <49911434+pippo6000@users.noreply.github.com>
2020-03-12 22:47:56 +0100

    Updates

---
+++ precache-manifest.0f924841d4012070844677afa4d7d1be.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c01ef96c233c4eba49bd032dedcf252e",
+    "url": "/pippo6000/My-Cocktail/index.html"
+  },
+  {
+    "revision": "3f85f31b43635d41e534",
+    "url": "/pippo6000/My-Cocktail/static/css/main.9c6423d6.chunk.css"
+  },
+  {
+    "revision": "745d37f3085aa3e052ed",
+    "url": "/pippo6000/My-Cocktail/static/js/2.42c47187.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/pippo6000/My-Cocktail/static/js/2.42c47187.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f85f31b43635d41e534",
+    "url": "/pippo6000/My-Cocktail/static/js/main.4a92259a.chunk.js"
+  },
+  {
+    "revision": "df7a7fae61b7b0a040f3",
+    "url": "/pippo6000/My-Cocktail/static/js/runtime-main.b3958bf4.js"
+  }
+]);
